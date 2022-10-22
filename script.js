51ca9e0bd78c0ec86167467bfb6691e5f131daee
your name <69403976+JasonTable@users.noreply.github.com>
2022-10-21 20:05:07 -0400

    added velocity download

--- script.js
+++ script.js
@@ -6,3 +6,12 @@ async function get_papermc(project,version) {
     console.log(latest);
     window.location = "https://papermc.io/api/v2/projects/"+project+"/versions/"+version+"/builds/"+latest+"/downloads/"+project+"-"+version+"-"+latest+".jar"
 }
+
+async function get_lastest_ver(project) {
+    let page = await fetch("https://papermc.io/api/v2/projects/"+project+"/");
+    let text = await page.text();
+    let data = JSON.parse(text);
+    let latest = data.versions[data.versions.length-1];
+    console.log(latest);
+    await get_papermc(project,latest);
+}
